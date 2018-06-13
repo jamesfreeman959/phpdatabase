4b632abe5eb8276c866fb2a3f09327f22f10d5c7
James Freeman <sysadmin@hkskies.com>
2018-06-13 14:34:01 +0100

    Modified dbtest.php to use MYSQL_ variables for consistency, and added a simple PHP file that dumps the first 10 rows of the employees sample database from MySQL

--- top10employeenames.php
+++ top10employeenames.php
@@ -11,6 +11,24 @@ if ($connection->connect_errno) {
     exit();
 } else {
     printf("Connected to the database");
+
+$result = mysqli_query($connection,"SELECT first_name,last_name FROM employees LIMIT 0,10;");
+
+echo "<table border='1'>
+<tr>
+<th>Firstname</th>
+<th>Lastname</th>
+</tr>";
+
+while($row = mysqli_fetch_array($result))
+{
+echo "<tr>";
+echo "<td>" . $row['first_name'] . "</td>";
+echo "<td>" . $row['last_name'] . "</td>";
+echo "</tr>";
+}
+echo "</table>";
+
 }
 $connection->close();
 ?>
