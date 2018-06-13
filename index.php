fcf70aae955b0bb8d2a72b680d3572f77a70052c
James Freeman <sysadmin@hkskies.com>
2018-06-13 16:20:58 +0100

    Tweaked formatting, added hostname of active pod for load balancing examples

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
-echo "Welcome to OpenShift Online Developer Preview";
+echo "<h1>Welcome to OpenShift Online Developer Preview</h1>";
+echo "Running on pod named: <b>" . gethostname() . "</b>";
 echo "<br>To test the database, hit the <a href=\"/dbtest.php\">dbtest.php</a> URL";
 echo "<br>If you are using the MySQL Employees Sample Database, go to <a href=\"/top10employeenames.php\">top10employeenames.php</a> to see the first 10 rows in the database";
 
