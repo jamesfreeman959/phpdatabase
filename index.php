234d73cb7d731ace595dadd43fdf1d91fa521809
James Freeman <sysadmin@hkskies.com>
2018-06-13 14:55:01 +0100

    Updated index.php to reference MYSQL_ variables, and include hyperlinks to the other two pages

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 
 echo "Welcome to OpenShift Online Developer Preview";
-echo "<br>To test the database, hit the dbtest.php URL";
+echo "<br>To test the database, hit the <a href=\"/dbtest.php\">dbtest.php</a> URL";
+echo "<br>If you are using the MySQL Employees Sample Database, go to <a href=\"/top10employeenames.php\">top10employeenames.php</a> to see the first 10 rows in the database";
 
 
 echo "<br><br>This assumes that you have the correct env variables set";
-echo "<BR><BR>The environment variables required are databaseuser, databasepassword, and databasenmae.";
+echo "<BR><BR>The environment variables required are MYSQL_USER, MYSQL_PASSWORD, and MYSQL_DATABASE";
 
